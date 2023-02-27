71e517e5225fde2441fe7d0046e927748ccc9c97
Etienne Robin <etienner0601@gmail.com>
2023-02-27 15:09:52 -0800

    finished part 2

---
+++ customdialogs.js
@@ -0,0 +1,62 @@
+const alertTemplate = document.getElementById("alert-dialog");
+const confirmTemplate = document.getElementById("confirm-dialog");
+const promptTemplate = document.getElementById("prompt-dialog");
+
+const alertBtn = document.getElementById("alert-button");
+const confirmBtn = document.getElementById("confirm-button");
+const promptBtn = document.getElementById("prompt-button");
+const outputBox = document.getElementById("output");
+
+alertBtn.addEventListener("click", () => {
+    outputBox.style.display = "none";
+    document.body.style.backgroundColor = "lightgray";
+    const alertNode = alertTemplate.content.cloneNode(true);
+    alertNode.firstElementChild.firstElementChild.lastElementChild.firstElementChild.addEventListener("click", function() {
+        document.body.style.backgroundColor = "white";
+    });
+    document.body.append(alertNode);
+});
+
+confirmBtn.addEventListener("click", function() {
+    outputBox.style.display = "none";
+    document.body.style.backgroundColor = "lightgray";
+
+    const confirmNode = confirmTemplate.content.cloneNode(true);
+    
+    const cancelBtn = confirmNode.firstElementChild.firstElementChild.lastElementChild.firstElementChild;
+    const okBtn = confirmNode.firstElementChild.firstElementChild.lastElementChild.lastElementChild;
+
+    cancelBtn.addEventListener("click", function() {
+        document.body.style.backgroundColor = "white";
+        outputBox.innerHTML = "Confirm result : false";
+        outputBox.style.display = "inline-block";
+    });
+    okBtn.addEventListener("click", function() {
+        document.body.style.backgroundColor = "white";
+        outputBox.innerHTML = "Confirm result : true";
+        outputBox.style.display = "inline-block";
+    });
+
+    document.body.append(confirmNode);
+});
+
+promptBtn.addEventListener("click", function() {
+    outputBox.style.display = "none";
+    document.body.style.backgroundColor = "lightgray";
+
+    const promptNode = promptTemplate.content.cloneNode(true);
+
+    const formNode = promptNode.firstElementChild.firstElementChild.firstElementChild;
+
+    formNode.addEventListener("submit", function() {
+        document.body.style.backgroundColor = "white";
+
+        let res = formNode.children[2].value;
+        let clean = DOMPurify.sanitize(res);
+        outputBox.innerHTML = clean ? `Prompt result : ${clean}` : "Prompt result : User didn't enter anything";
+        outputBox.style.display = "inline-block";
+    });
+
+    document.body.append(promptNode);
+});
+
